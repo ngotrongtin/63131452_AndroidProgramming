39136a150cbc3171f0564dc983d9b56acca84eb0
ngotrongtin <tigopro.1703@gmail.com>
2024-03-25 14:12:27 +0700

    cau 2 severity

--- ntu_63131452/Cau2_AppBMI/app/src/main/java/com/example/appbmi/MainActivity.java
+++ ntu_63131452/Cau2_AppBMI/app/src/main/java/com/example/appbmi/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.appbmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private String severity(double bmi) {
+        if (bmi < 18.5) {
+            return "Cân nặng thấp";
+        } else if (bmi >= 18.5 && bmi < 25) {
+            return "Bình thường";
+        } else if (bmi >= 25 && bmi < 30) {
+            return "Tiền béo phì";
+        } else {
+            return "Béo phì";
+        }
+    }
+
 }
