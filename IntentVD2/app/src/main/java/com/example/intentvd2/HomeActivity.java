c548be88cf912e774d274ac34df2e533007a4798
ngotrongtin <tigopro.1703@gmail.com>
2024-04-26 14:05:09 +0700

    intent VD2 completed

--- IntentVD2/app/src/main/java/com/example/intentvd2/HomeActivity.java
+++ IntentVD2/app/src/main/java/com/example/intentvd2/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.intentvd2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        TextView userNameOutput = findViewById(R.id.nameLabel);
+        Intent Ilogin = getIntent();
+        String userName = Ilogin.getStringExtra("userName");
+
+        userNameOutput.setText(userName);
     }
 }
--- IntentVD2/app/src/main/java/com/example/intentvd2/LoginActivity.java
+++ IntentVD2/app/src/main/java/com/example/intentvd2/LoginActivity.java
@@ -2,13 +2,41 @@ package com.example.intentvd2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    String dbName = "ngotrongtin";
+    String dbPassword = "password";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void LoginCheck(View v){
+        EditText userNameInput = findViewById(R.id.UserNameInput);
+        EditText passwordInput = findViewById(R.id.PasswordInput);
+        TextView notifyAreaOutput = findViewById(R.id.NotifyArea);
+        String userNameInputStr = userNameInput.getText().toString();
+        String passwordInputStr = passwordInput.getText().toString();
+
+        if(!userNameInputStr.equals(dbName) || !passwordInputStr.equals(dbPassword)){
+            notifyAreaOutput.setText("Mật khẩu hoặc tên không hợp lệ");
+            return;
+        }
+
+
+        Intent IHome = new Intent(this, HomeActivity.class);
+        IHome.putExtra("userName", userNameInputStr);
+        IHome.putExtra("password", passwordInputStr);
+
+        startActivity(IHome);
+    }
+
+
 }
