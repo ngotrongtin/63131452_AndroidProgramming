c2a2b9cb2567875eece1205403a0e94eac8d1018
ngotrongtin <tigopro.1703@gmail.com>
2024-05-09 18:41:46 +0700

    view pager2 completed

--- ViewPager2/app/src/main/java/com/example/viewpager/MainActivity.java
+++ ViewPager2/app/src/main/java/com/example/viewpager/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.viewpager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> viewPagerData;
+    ViewPager2 viewPager2Land;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPagerData = getDataForViewPager();
+        viewPager2Land = findViewById(R.id.vp2Land);
+        landScapeAdapter = new LandScapeAdapter(this, viewPagerData);
+        viewPager2Land.setAdapter(landScapeAdapter);
+        viewPager2Land.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
+    }
+
+    ArrayList<LandScape> getDataForViewPager(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("anhmeocute1", "ảnh meo cute 1");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("anhmeocute2", "ảnh mèo con cute 2"));
+        dsDuLieu.add(new LandScape("anhmeocute3", "ảnh mèo con cute 3"));
+        return dsDuLieu;
     }
 }
